b0c4b2b475183c760c7dd684f2970fde49876cea
Jacob <thejacobcohen@gmail.com>
2025-09-04 11:31:21 -0400

    Updated Protocol structure to make sure no padding is adding and renamed vairbale for consistency

--- paxi_ws/src/paxi_hardware/include/paxi_hardware/hoverboard_protocol_struct.hpp
+++ paxi_ws/src/paxi_hardware/include/paxi_hardware/hoverboard_protocol_struct.hpp
@@ -7,27 +7,30 @@ constexpr uint16_t k_start_frame = 0xABCD;
 #include <cstdint>
 #include <utility>
 
-typedef struct {
-   uint16_t start{k_start_frame};
-   int16_t  steer{0};
-   int16_t  speed{0};
-   uint16_t checksum{0};
-} SerialCommand;
 
-typedef struct {
-   uint16_t start{k_start_frame};
-   int16_t  cmd1{0};
-   int16_t  cmd2{0};
-   int16_t  speedR_meas{0};
-   int16_t  speedL_meas{0};
-   int16_t  wheelR_cnt{0};
-   int16_t  wheelL_cnt{0}; 
-   int16_t  left_dc_curr{0};
-   int16_t  right_dc_curr{0};   
-   int16_t  batVoltage{0};
-   int16_t  boardTemp{0};
-   uint16_t cmdLed{0};
-   uint16_t checksum{0};
-} SerialFeedback;
+
+
+struct __attribute__((packed)) SerialCommand {
+    uint16_t start;
+    int16_t steer;
+    int16_t speed;
+    uint16_t checksum;
+};
+
+struct __attribute__((packed)) SerialFeedback {
+    uint16_t start;
+    int16_t cmd_l;
+    int16_t cmd_r;
+    int16_t speed_r_meas;
+    int16_t speed_l_meas;
+    int16_t wheel_r_cnt;
+    int16_t wheel_l_cnt;
+    int16_t left_dc_curr;
+    int16_t right_dc_curr;
+    int16_t bat_voltage;
+    int16_t board_temp;
+    uint16_t cmd_led;
+    uint16_t checksum;
+};
 
 #endif
