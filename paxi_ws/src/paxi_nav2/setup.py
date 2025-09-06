bbaac8311745b6dd97218b9a256e76651743d8c1
Jacob <thejacobcohen@gmail.com>
2025-09-06 18:20:37 -0400

    started organizing nav2 package

---
+++ paxi_ws/src/paxi_nav2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'paxi_nav2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='j',
+    maintainer_email='thejacobcohen@gmail.com',
+    description='Nav2 launch files for autonomous navigation and mappping',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
