3530dc789a8eaab5214010eea45cabc3864ef7ca
Jacob <thejacobcohen@gmail.com>
2025-09-07 14:28:46 -0400

    fixed some typos in bash script

--- scripts/paxi_dependencies.bash
+++ scripts/paxi_dependencies.bash
@@ -1,51 +1,47 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+set -e
 
-
-
-if[[-z ${ROS_DISTRO}]]; then
-	distro=${ROS_DISTRO}
+# Determine ROS distro
+if [[ -n "${ROS_DISTRO}" ]]; then
+    distro=${ROS_DISTRO}
 else
-	distro = "humble
+    distro="humble"
 fi
 
-
-echo "Installing ros ${distro} packages that paxi_bot depends on"
+echo "Installing ROS ${distro} packages that paxi_bot depends on"
 
 sudo apt update
 
 dependencies_ros_packages=(
-     ros-$distro-ament-cmake
-     ros-$distro-navigation2
-     ros-$distro-nav2-bringup
-     ros-$distro-robot-localization
-     ros-$distro-slam-toolbox
-     # ros-$distro-joint-state-publisher-guo
-     ros-$distro-ros2-control
-     ros-$distro-ros2-controllers
+    ros-"$distro"-ament-cmake
+    ros-"$distro"-navigation2
+    ros-"$distro"-nav2-bringup
+    ros-"$distro"-robot-localization
+    ros-"$distro"-slam-toolbox
+    # ros-"$distro"-joint-state-publisher-gui
+    ros-"$distro"-ros2-control
+    ros-"$distro"-ros2-controllers
 )
 
-sucessful_install_packages=()
+successful_install_packages=()
 failed_install_packages=()
 
-sudo apt install -y "${dependencies_ros_packages[0]}"
-
-for pkg in "${dependencies_ros_packages[0]}";do
-	echo "Attemping to install package $pkg"
-	sudo apt install -y $pkg
-	
-	if[ $? -ne 0]; then
-		failed_instaled_packages+=($pkg)
-		echo "Failed to install package $pkg"
-	else
-		sucessful_install_packages+=($pkg)
-		echo "Sucessfully installed package $pkg"
+for pkg in "${dependencies_ros_packages[@]}"; do
+    echo "Attempting to install package $pkg"
+    if sudo apt install -y "$pkg"; then
+        successful_install_packages+=("$pkg")
+        echo "Successfully installed package $pkg"
+    else
+        failed_install_packages+=("$pkg")
+        echo "Failed to install package $pkg"
+    fi
 done
 
-if [${#failed_install_packages} -ne 0];then
-	echo "Failed to install $(#failed_install_packages") ros packages: "
-	echo "${failed_insall_packages[0]}"
+if [[ ${#failed_install_packages[@]} -ne 0 ]]; then
+    echo "Failed to install ${#failed_install_packages[@]} ROS packages:"
+    printf '%s\n' "${failed_install_packages[@]}"
+fi
 
-echo "Sucessfully installed $(#sucessful_install_packages") ros packages:"
-echo "${sucessful_insall_packages[0]}"
-
+echo "Successfully installed ${#successful_install_packages[@]} ROS packages:"
+printf '%s\n' "${successful_install_packages[@]}"
 
