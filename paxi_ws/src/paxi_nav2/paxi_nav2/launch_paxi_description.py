2532f26af9a4c56a302e04e09a02852cf5e011ac
Jacob <thejacobcohen@gmail.com>
2025-09-07 12:50:51 -0400

    added to default rviz config to automatically display robot model and tf

--- paxi_ws/src/paxi_nav2/paxi_nav2/launch_paxi_description.py
+++ paxi_ws/src/paxi_nav2/paxi_nav2/launch_paxi_description.py
@@ -1,7 +1,7 @@
 
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
@@ -16,6 +16,7 @@ def generate_launch_description():
 
     default_model_path = os.path.join(pkg_share, 'urdf', 'paxi_bot.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'paxi_bot.rviz')
+    world_path=os.path.join(pkg_share, 'world' ,'my_world.sdf')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -46,10 +47,26 @@ def generate_launch_description():
         parameters = [os.path.join(pkg_share,'config/nav2_ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
+    # lidar_node = Node(
+    #     package='rplidar_ros',  # Change to your lidar package
+    #     executable='rplidar_node',  # Change to your lidar executable
+    #     name='rplidar_node',
+    #     output='screen',
+    #     parameters=[{
+    #         'serial_port': '/dev/ttyUSB0',  # Adjust to your lidar port
+    #         'frame_id': 'laser_frame',
+    #         'angle_compensate': True,
+    #         'scan_mode': 'Standard',
+    #         'use_sim_time': use_sim_time
+    #     }],
+    #     condition=IfCondition(LaunchConfiguration('use_lidar'))
+    # )
+
     return LaunchDescription([
         DeclareLaunchArgument(name='use_sim_time', default_value='False', description= 'Flag to enable use_sim_time'),
         DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
         DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        #ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         joint_state_publisher_node,
         robot_state_publisher_node,
         robot_localization_node,
