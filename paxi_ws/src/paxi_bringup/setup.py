456418c472e6bed5b3f1188fef135ce6d37ed947
Jacob <thejacobcohen@gmail.com>
2025-09-11 16:06:45 -0400

    Move launch controller to paxi_bringup

--- paxi_ws/src/paxi_bringup/setup.py
+++ paxi_ws/src/paxi_bringup/setup.py
@@ -1,10 +1,10 @@
+from setuptools import find_packages, setup
+
 import os
 from glob import glob
 
-from setuptools import find_packages, setup
 
-package_name = 'launch_paxi_controller'
-
+package_name = 'paxi_bringup'
 
 setup(
     name=package_name,
@@ -14,7 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch_paxi_controller'), glob('launch_paxi_controller/*.py')),
+        (os.path.join('share', package_name, 'paxi_bringup'), glob('paxi_bringup/*.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share', package_name, 'controller'), glob('controller/*.yaml'))
     ],
@@ -22,7 +22,7 @@ setup(
     zip_safe=True,
     maintainer='j',
     maintainer_email='thejacobcohen@gmail.com',
-    description='Launch ros2_control differential drive for the paxibot',
+    description='launch files for paxi',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
