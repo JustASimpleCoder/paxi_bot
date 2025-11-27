02693e6eae2183f4e9120707a00ad92989ca6c93
Jacob <thejacobcohen@gmail.com>
2025-11-26 22:10:15 -0500

    Removed reammping to remove ros warning

--- paxi_ws/src/paxi_bringup/paxi_bringup/main_bringup.py
+++ paxi_ws/src/paxi_bringup/paxi_bringup/main_bringup.py
@@ -36,12 +36,8 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controller_config],
-        output="both",        
-        remappings=[
-            ("~/robot_description", "/robot_description"),
-        ],
-
+        parameters=[robot_controller_config],
+        output="both",
     )
 
     robot_state_pub_node = Node(
