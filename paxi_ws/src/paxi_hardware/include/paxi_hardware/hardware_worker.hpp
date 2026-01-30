b54a7917afbab1ffd7118c2e25570a6cad4472d6
Jacob <thejacobcohen@gmail.com>
2026-01-30 14:49:31 -0500

    Script to save nav2 map from live async mapping

--- paxi_ws/src/paxi_hardware/include/paxi_hardware/hardware_worker.hpp
+++ paxi_ws/src/paxi_hardware/include/paxi_hardware/hardware_worker.hpp
@@ -115,17 +115,16 @@ private:
 
   std::unique_ptr<PaxiInterfaceNode> paxi_interface_node_;
   rclcpp::Clock::SharedPtr cached_clock_;
-
-  void worker_loop();
-  void no_data_handler(const rclcpp::Time & now);
-  void disconnected_handler(const rclcpp::Time & now);
-
   const sensor_msgs::msg::Imu & update_paxi_interface_state();
   size_t no_data_read_count_;
   size_t disconnect_read_count_;
 
   rclcpp::Time no_data_last_time_;
   rclcpp::Time disconnect_read_time_;
+
+  void worker_loop();
+  void no_data_handler(const rclcpp::Time & now);
+  void disconnected_handler(const rclcpp::Time & now);
 
   ssize_t get_new_feedback_buffer();
   void protocol_parsing_loop(const ssize_t bytes_read);
---
+++ scripts/save_nav2_map.sh
@@ -0,0 +1,5 @@
+echo "You must be running ros2 slam_toolbox_launch and nav2_async_live_mapping_launch"
+echo "To run live mapping for paxi_bot, run live_mapping_launch.sh in the /paxi_bot/scripts directiory"
+
+
+
