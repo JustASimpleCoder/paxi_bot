69a687921dee29f488fab37e3144828f68200a9d
Jacob <thejacobcohen@gmail.com>
2026-02-07 17:15:15 -0500

    Updated copyrights, removed flake 8 warning with pytest.ini and passed all ros2 tests

--- paxi_ws/src/paxi_hardware/include/paxi_hardware/paxi_interface.hpp
+++ paxi_ws/src/paxi_hardware/include/paxi_hardware/paxi_interface.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 JustASimpleCoder
+// Copyright 2026 JustASimpleCoder
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
