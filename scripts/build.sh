a1d3ecb9ac21c439708870829e61ce9bd1476b5f
Jacob <thejacobcohen@gmail.com>
2026-02-26 23:39:27 -0500

    Added printf instead of echo to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,28 +34,28 @@ NC=$'\e[0m'
 
 cd $WORKSPACE_DIR
 
-echo "\nInstalling vcstool\n\n"
+printf "\nInstalling vcstool\n\n"
 
 sudo apt install python3-vcstool
 
-echo "\nImporting repos from paxi.repos\n\n"
+printf "\nImporting repos from paxi.repos\n\n"
 vcs import src < src/paxi.repos
 
 
 cd  $SLLIDAR_DIR_SCRIPTS
 
-echo "\ncreating udev rules for sllidar \n\n"
+printf "\ncreating udev rules for sllidar \n\n"
 sudo ./create_udev_rule.sh
 
 cd $SLAMKIT_DIR_SCRIPTS
-echo "\nCreating udev rules for slamtec \n\n"
+printf "\nCreating udev rules for slamtec \n\n"
 sudo ./add_udev.sh
 
 cd $SLAMKIT_DIR
-echo "\nCreating udev rules for slamtec \n\n"
+printf "\nCreating udev rules for slamtec \n\n"
 git submodule update --init
 
 cd $WORKSPACE_DIR
-echo "\nInstalling ROS2 dependencies \n\n"
+printf "\nInstalling ROS2 dependencies \n\n"
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
