e8e9e633191a86df6b297ce19f7e3ed7d517648b
Jacob <thejacobcohen@gmail.com>
2026-03-08 03:10:28 -0400

    Reformat to ROS standard for hardware bag stuff

--- paxi_ws/src/paxi_data_collection/src/hardware_bag.cpp
+++ paxi_ws/src/paxi_data_collection/src/hardware_bag.cpp
@@ -9,10 +9,10 @@ HardwareBag::HardwareBag()
   hardware_writer_ = std::make_unique<rosbag2_cpp::Writer>();
 
   params_ = param_listener_->get_params();
-  
+
   std::string bag_name = params_.bag_name;
 
-  if(get_parameter("stamp_current_date").as_bool()){
+  if (params_.stamp_current_date) {
     bag_name = bag_name + "_" + get_current_date();
   }
 
@@ -21,46 +21,49 @@ HardwareBag::HardwareBag()
   std::vector<std::string> topic_names = params_.topic_names;
   std::vector<std::string> topic_msg_types = params_.topic_msg_types;
   std::vector<std::string> topic_qos = params_.topic_msg_qos_types;
+
   std::size_t num_of_topic = topic_names.size();
 
-  if(num_of_topic != topic_msg_types.size()){
+  if (num_of_topic != topic_msg_types.size()) {
     RCLCPP_FATAL(
       get_logger(),
       "Topic names size [%zu] is not equal to topic_msg_types [%zu]",
       topic_names.size(), topic_msg_types.size()
     );
 
-    throw std::runtime_error("Parameter size mismatch, please ensure topic_names and "
-      "topic_msg_types have the same size (and match the correct type by indec)");
+    throw std::runtime_error(
+            "Parameter size mismatch, please ensure topic_names and "
+            "topic_msg_types have the same size (and match the correct type by indec)");
   }
 
-  if(num_of_topic != topic_qos.size()){
+  if (num_of_topic != topic_qos.size()) {
     RCLCPP_FATAL(
       get_logger(),
       "Topic names size [%zu] is not equal to topic_qos [%zu]",
       topic_names.size(), topic_msg_types.size()
     );
 
-    throw std::runtime_error("Parameter size mismatch, please ensure topic_names and "
-      "topic_qos have the same size (and match the correct type by indec)");
+    throw std::runtime_error(
+            "Parameter size mismatch, please ensure topic_names and "
+            "topic_qos have the same size (and match the correct type by indec)");
   }
 
   auto make_callback = [this](const std::string & topic_name, const std::string & topic_msg_type) {
-    return [this, topic_name, topic_msg_type](std::shared_ptr<rclcpp::SerializedMessage> msg) {
-              hardware_writer_->write(msg, topic_name, topic_msg_type, this->now());
-            };
-  };
+      return [this, topic_name, topic_msg_type](std::shared_ptr<rclcpp::SerializedMessage> msg) {
+               hardware_writer_->write(msg, topic_name, topic_msg_type, this->now());
+             };
+    };
 
   auto get_qos = [](std::string & qos_str)-> rclcpp::QoS {
-      if(qos_str == "sensor"){
+      if (qos_str == "sensor") {
         return rclcpp::SensorDataQoS();
-     
       }
       return rclcpp::SystemDefaultsQoS();
-  };
+    };
 
   subscriptions_.reserve(num_of_topic);
-  for(std::size_t i = 0; i < num_of_topic; ++i){
+
+  for (std::size_t i = 0; i < num_of_topic; ++i) {
 
     const std::string & topic_name = topic_names[i];
     const std::string & topic_msg_type = topic_msg_types[i];
@@ -78,16 +81,15 @@ HardwareBag::HardwareBag()
   }
 }
 
-
 const std::string HardwareBag::get_current_date() const
 {
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-    struct tm* parts = std::localtime(&now_c);
+  auto now = std::chrono::system_clock::now();
+  std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+  struct tm * parts = std::localtime(&now_c);
 
-    std::stringstream ss;
-    ss << std::put_time(parts, "%Y-%m-%d");
-    std::string date_str = ss.str();
-    return date_str;
+  std::stringstream ss;
+  ss << std::put_time(parts, "%Y-%m-%d");
+  std::string date_str = ss.str();
+  return date_str;
 }
 }  // namespace paxi_data_collection
