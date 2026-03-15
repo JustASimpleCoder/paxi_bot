59af9697f2bd620fb0c189b3ad86df90bd9faa2c
Jacob <thejacobcohen@gmail.com>
2026-03-15 18:23:50 -0400

    Begin basic statistics on gyro from imu and pose comparisons with hoverboard odom

--- paxi_ws/src/paxi_data_analysis/paxi_data_analysis/calculate_gyro_bias.py
+++ paxi_ws/src/paxi_data_analysis/paxi_data_analysis/calculate_gyro_bias.py
@@ -19,43 +19,42 @@ import matplotlib.pyplot as plt
 
 
 imu_q_names = {
-   "q_x": "orientation.x",
-   "q_y": "orientation.y",
-   "q_z": "orientation.z",
-   "q_w": "orientation.w",
-   "g_x": "angular_velocity.x",
-   "g_y": "angular_velocity.y",
-   "g_z": "angular_velocity.z",
-   "a_x": "linear_acceleration.x",
-   "a_y": "linear_acceleration.y",
-   "a_z": "linear_acceleration.z",
-} 
+    "q_x": "orientation.x",
+    "q_y": "orientation.y",
+    "q_z": "orientation.z",
+    "q_w": "orientation.w",
+    "g_x": "angular_velocity.x",
+    "g_y": "angular_velocity.y",
+    "g_z": "angular_velocity.z",
+    "a_x": "linear_acceleration.x",
+    "a_y": "linear_acceleration.y",
+    "a_z": "linear_acceleration.z",
+}
 
 header_sec = "header.stamp.sec"
 header_nano_sec = "header.stamp.nanosec"
 
+
 def main():
 
-  #TODO(Jacob): standardize this so we dont pass full path -> on main branch will be located differently
-  # Just ened to figure out where we want to place bags from sensor bags and using ros2-unbag
-  # where to place .csv files
-  imu_data_flename = "imu_data.csv"
-  folder = "~/robotics/paxi_bot_dev/paxi_bot/paxi_ws/sensors_bag_test_ACML_default_3_2026-03-14"
-  full_path_imu = os.path.join(folder + "/imu_data", imu_data_flename)
+    # TODO(Jacob): standardize this so we dont pass full path -> on main branch will be located differently
+    # Just ened to figure out where we want to place bags from sensor bags and using ros2-unbag
+    # where to place .csv files
+    imu_data_flename = "imu_data.csv"
+    folder = "~/robotics/paxi_bot_dev/paxi_bot/paxi_ws/sensors_bag_test_ACML_default_3_2026-03-14"
+    full_path_imu = os.path.join(folder + "/imu_data", imu_data_flename)
 
-  imu_slamtec_df = pd.read_csv(full_path_imu)
+    imu_slamtec_df = pd.read_csv(full_path_imu)
 
-  gyros_stationary_ = imu_slamtec_df.loc[
-      (imu_slamtec_df[imu_q_names["g_z"]] < 0.02) & 
-      (imu_slamtec_df[imu_q_names["g_z"]] > -0.02), 
-      imu_q_names["g_z"]
-   ]
-  gyros_stationary_ = imu_slamtec_df[imu_q_names["g_z"]].between(-0.02, 0.02)
-
-  #
-  print(gyros_stationary_.describe())
-  print(f"Mean is [{gyros_stationary_.mean()}]")
+    gyros_stationary_ = imu_slamtec_df.loc[
+        (imu_slamtec_df[imu_q_names["g_z"]] < 0.01)
+        & (imu_slamtec_df[imu_q_names["g_z"]] > -0.02),
+        imu_q_names["g_z"],
+    ]
+    
+    print(gyros_stationary_.describe())
+    print(f"Mean is [{gyros_stationary_.mean()}]")
 
 
-if __name__ == '__main__':
-     main()
+if __name__ == "__main__":
+    main()
