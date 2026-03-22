89f805309d247fac85a169558fe727ce33ee92d4
Jacob <thejacobcohen@gmail.com>
2026-03-21 22:51:51 -0400

    Python pacakge init for faster bag2csv scripts

---
+++ paxi_ws/src/paxi_bag_to_csv/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'paxi_bag_to_csv'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='j',
+    maintainer_email='thejacobcohen@gmail.com',
+    description='Converts ros2bag into csv',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'bag2csv = paxi_bag_to_csv.bag2csv:main'
+        ],
+    },
+)
