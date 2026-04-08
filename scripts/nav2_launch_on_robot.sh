032376ca5a4b831e8ba515ef0e93701b2fc5b71a
Jacob <thejacobcohen@gmail.com>
2026-04-07 21:01:15 -0400

    Added -e flag to pass ekf for testing differend ekf config files

--- scripts/nav2_launch_on_robot.sh
+++ scripts/nav2_launch_on_robot.sh
@@ -46,6 +46,42 @@ LAUNCH_FILES=(
     "nav2.py"
 )
 
+NAV2_ARGS=(
+    "ekf_filename:="
+)
+
+ADD_EKF_ARGUMENT=false
+EKF_FILENAME=""
+
+while getopts "he:" opt; do
+  case "$opt" in 
+  h)
+    echo "usage"
+    echo "$0                      Launch tmux with main bringup and nav2 launch in split windows "
+    echo "$0 -e <eff_filename>    Lunach with a different ekf file sent"
+    exit 0
+    ;;
+  e)
+    ADD_EKF_ARGUMENT=true
+    EKF_FILENAME=$OPTARG
+    ;;
+  *)
+    echo "Invalid usage, flag not recognized"
+    echo "Try $0 -h for help"
+    exit 1
+    ;;
+  esac
+done
+
+
+
+if [[ "$ADD_EKF_ARGUMENT" == true ]]; then
+    LAUNCH_FILES[0]+=" "
+    LAUNCH_FILES[0]+="${NAV2_ARGS[0]}"
+    LAUNCH_FILES[0]+="$EKF_FILENAME"
+fi
+
+
 LAUNCH_FILE_NUM=${#LAUNCH_FILES[@]}
 
 #check if session already exist, kill it if it does
@@ -69,7 +105,6 @@ tmux select-layout -t "$SESSION":"$WINDOW" tiled
 declare -i launch_file_ptr=0
 for pane in $(tmux list-panes -F '#P'); do
     tmux send-keys -t "$SESSION:$WINDOW.$pane" "${ROS_COMMANDS[0]}" C-m
-
     tmux send-keys -t "$SESSION:$WINDOW.$pane" "${ROS_COMMANDS[1]} ${LAUNCH_FILES[$launch_file_ptr]}" C-m
     
     launch_file_ptr+=1
