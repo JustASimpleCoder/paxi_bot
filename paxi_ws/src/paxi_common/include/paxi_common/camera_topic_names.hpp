fa707d57216677b356e65dfe3a196e94afe928c8
Jacob <thejacobcohen@gmail.com>
2026-04-28 00:25:12 -0400

    Reformat cmaera topic names in paxi_common to meet ros standards

--- paxi_ws/src/paxi_common/include/paxi_common/camera_topic_names.hpp
+++ paxi_ws/src/paxi_common/include/paxi_common/camera_topic_names.hpp
@@ -18,35 +18,39 @@
 namespace paxi_common::camera_topic_names
 {
 
-  // camera info topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_RGB_CAMERA_INFO_TOPIC = "/driver/rgb/camera_info";
+// camera info topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_RGB_CAMERA_INFO_TOPIC = "/driver/rgb/camera_info";
 
-  // RGB image raw topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_RGB_IMAGE_RAW_TOPIC = "/driver/rgb/image_raw";
+// RGB image raw topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_RGB_IMAGE_RAW_TOPIC = "/driver/rgb/image_raw";
 
-  // RGB Compressed image raw topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_RGB_COMPRESSED_TOPIC = "/driver/rgb/image_raw/compressed";
+// RGB Compressed image raw topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_RGB_COMPRESSED_TOPIC = "/driver/rgb/image_raw/compressed";
 
-  // RGB Compressed Depth topic  topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_RGB_COMPRESSED_DEPTH_TOPIC ="/driver/rgb/image_raw/compressedDepth";
+// RGB Compressed Depth topic  topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_RGB_COMPRESSED_DEPTH_TOPIC =
+  "/driver/rgb/image_raw/compressedDepth";
 
-  // RBG THEORA image raw topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_RGB_THEORA_TOPIC = "/driver/rgb/image_raw/theora ";
+// RBG THEORA image raw topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_RGB_THEORA_TOPIC = "/driver/rgb/image_raw/theora ";
 
-  // Stero image raw topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_STEREO_CAMERA_INFO_TOPIC ="/driver/stereo/camera_info ";
+// Stero image raw topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_STEREO_CAMERA_INFO_TOPIC = "/driver/stereo/camera_info ";
 
-  // Stereo image raw topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_STEREO_RAW_TOPIC = "/driver/stereo/image_raw ";
+// Stereo image raw topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_STEREO_RAW_TOPIC = "/driver/stereo/image_raw ";
 
-  // Stereo  name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_STEREO_COMPRESSED_TOPIC = "/driver/stereo/image_raw/compressed";
+// Stereo  name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_STEREO_COMPRESSED_TOPIC =
+  "/driver/stereo/image_raw/compressed";
 
-  // Stereo info topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_STEREO_COMPRESSED_DEPTH_TOPIC = "/driver/stereo/image_raw/compressedDepth";
+// Stereo info topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_STEREO_COMPRESSED_DEPTH_TOPIC =
+  "/driver/stereo/image_raw/compressedDepth";
 
-  // Stereo THEORA info topic name from OAK_D_LIT ros2 brdige
-  inline constexpr const char * OAK_D_LITE_STEREO_THEORA_RAW_TOPIC = "/driver/stereo/image_raw/theora";
+// Stereo THEORA info topic name from OAK_D_LIT ros2 brdige
+inline constexpr const char * OAK_D_LITE_STEREO_THEORA_RAW_TOPIC =
+  "/driver/stereo/image_raw/theora";
 }  // namespace paxi_common::hardware_topics
 
 #endif  // PAXI_COMMON__HARDWARE_TOPIC_NAMES_HPP_
